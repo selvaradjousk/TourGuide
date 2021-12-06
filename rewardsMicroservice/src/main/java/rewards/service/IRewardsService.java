f5dca256fb33f40ee14df92c5cd5bc049f94d994
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-12-06 13:07:56 +0100

    [FEAT] rewardsMicroservice - testGetAttractionRewardPoints >  Implemented

--- rewardsMicroservice/src/main/java/rewards/service/IRewardsService.java
+++ rewardsMicroservice/src/main/java/rewards/service/IRewardsService.java
@@ -2,8 +2,18 @@ package rewards.service;
 
 import java.util.UUID;
 
+/**
+ * The Interface IRewardsService.
+ */
 public interface IRewardsService {
 
+	/**
+	 * Gets the attraction reward points.
+	 *
+	 * @param attractionId the attraction id
+	 * @param userId the user id
+	 * @return the attraction reward points
+	 */
 	int getAttractionRewardPoints(UUID attractionId, UUID userId);
 
 }
--- rewardsMicroservice/src/main/java/rewards/service/RewardsService.java
+++ rewardsMicroservice/src/main/java/rewards/service/RewardsService.java
@@ -2,15 +2,72 @@ package rewards.service;
 
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import rewardCentral.RewardCentral;
+
+/**
+ * The Class RewardsService.
+ */
 @Service
 public class RewardsService implements IRewardsService {
 
-	@Override
-	public int getAttractionRewardPoints(UUID attractionId, UUID userId) {
-		// TODO Auto-generated method stub
-		return 0;
+
+	/** The logger. */
+	private Logger logger = LoggerFactory
+			.getLogger(RewardsService.class);
+
+
+	 /** The reward central. */
+ 	private final RewardCentral rewardCentral;
+
+
+
+	// ##############################################################
+
+	/**
+	 * Instantiates a new rewards service.
+	 *
+	 * @param rewardCentral the reward central
+	 */
+	public RewardsService(RewardCentral rewardCentral) {
+		super();
+		this.rewardCentral = rewardCentral;
 	}
+
+
+
+
+	// ##############################################################
+
+
+
+    /**
+	 * Gets the attraction reward points.
+	 *
+	 * @param attractionId the attraction id
+	 * @param userId the user id
+	 * @return the attraction reward points
+	 */
+	public int getAttractionRewardPoints(
+    		final UUID attractionId,
+    		final UUID userId) {
+
+    	logger.debug("RewardsService.getAttractionRewardPoints called");
+
+        int rewardPoints = rewardCentral.getAttractionRewardPoints(
+        		attractionId,
+        		userId);
+
+        return rewardPoints;
+    }
+
+
+
+	// ##############################################################
+
+
 
 }
